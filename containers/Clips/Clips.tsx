876a60fb1dd8fca7b3a98be0714d2ff44f4f78a8
0xbono <iam@sonjim.in>
2021-08-09 14:53:21 +0900

    feat: MediaStream 활용한 카메라 온/오프 기능 추가

--- containers/Clips/Clips.tsx
+++ containers/Clips/Clips.tsx
@@ -1,3 +1,50 @@
+import { useState, useEffect, useRef } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Clips.module.scss';
+
+const cx = classNames.bind(styles);
+
 export const Clips: React.FC = () => {
-  return <></>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      (videoRef.current as HTMLVideoElement).srcObject = stream;
+    });
+  }, []);
+
+  const availableDevices = () => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      return devices
+        .filter((device) => device.kind === 'videoinput')
+        .forEach((device) => {
+          console.log(device);
+        });
+    });
+  };
+
+  const off = () => {
+    const stream = (videoRef.current as HTMLVideoElement).srcObject as any;
+    stream.getVideoTracks().forEach((track: any) => {
+      track.stop();
+      stream.srcObject = null;
+    });
+  };
+
+  const on = () => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      (videoRef.current as HTMLVideoElement).srcObject = stream;
+    });
+  };
+
+  return (
+    <section className={cx('wrap')}>
+      <video ref={videoRef} className={cx('webcam')} autoPlay playsInline muted />
+      <button className={cx('button')} onClick={off}>
+        OFF 버튼임!
+      </button>
+      <button className={cx('button')} onClick={on}>
+        ON 버튼임!
+      </button>
+    </section>
+  );
 };
