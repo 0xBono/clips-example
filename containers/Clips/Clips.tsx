68fd590c25ce138793238e3863a53ca0119b947e
0xbono <iam@sonjim.in>
2021-08-10 09:48:39 +0900

    feat(containers/clips): 카메라 온/오프 버튼 및 주석 추가

--- containers/Clips/Clips.tsx
+++ containers/Clips/Clips.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Clips.module.scss';
 
@@ -12,17 +12,19 @@ export const Clips: React.FC = () => {
     });
   }, []);
 
-  const availableDevices = () => {
-    navigator.mediaDevices.enumerateDevices().then((devices) => {
-      return devices
-        .filter((device) => device.kind === 'videoinput')
-        .forEach((device) => {
-          console.log(device);
-        });
+  /**
+   * @description 카메라 온/오프 과정에서 필요로 하는 MediaTracks 생성하는 함수
+   */
+  const startMediaTracks = () => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      (videoRef.current as HTMLVideoElement).srcObject = stream;
     });
   };
 
-  const off = () => {
+  /**
+   * @description 카메라 온/오프 과정에서 생기는 MediaTracks 제거하는 함수
+   */
+  const stopMediaTracks = () => {
     const stream = (videoRef.current as HTMLVideoElement).srcObject as any;
     stream.getVideoTracks().forEach((track: any) => {
       track.stop();
@@ -30,20 +32,14 @@ export const Clips: React.FC = () => {
     });
   };
 
-  const on = () => {
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-      (videoRef.current as HTMLVideoElement).srcObject = stream;
-    });
-  };
-
   return (
     <section className={cx('wrap')}>
       <video ref={videoRef} className={cx('webcam')} autoPlay playsInline muted />
-      <button className={cx('button')} onClick={off}>
-        OFF 버튼임!
+      <button className={cx('button')} onClick={stopMediaTracks}>
+        OFF
       </button>
-      <button className={cx('button')} onClick={on}>
-        ON 버튼임!
+      <button className={cx('button')} onClick={startMediaTracks}>
+        ON
       </button>
     </section>
   );
